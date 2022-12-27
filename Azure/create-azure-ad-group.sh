5d43c32061b42eab03faf5c07fa591042804aebe
tangtuphuc <119038678+tangtuphuc@users.noreply.github.com>
2022-12-27 15:14:50 +0700

    Changing Azure AD Group named

--- Azure/create-azure-ad-group.sh
+++ Azure/create-azure-ad-group.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-AZURE_AD_GROUP_NAME="devopsthehardway-aks-group"
+AZURE_AD_GROUP_NAME="DEVOPS"
 CURRENT_USER_OBJECTID=$(az ad signed-in-user show --query objectId -o tsv)
 
 # Create Azure AD Group
@@ -9,6 +9,6 @@ az ad group create --display-name $AZURE_AD_GROUP_NAME --mail-nickname $AZURE_AD
 # Add Current az login user to Azure AD Group
 az ad group member add --group $AZURE_AD_GROUP_NAME --member-id $CURRENT_USER_OBJECTID
 
-AZURE_GROUP_ID=$(az ad group show --group "devopsthehardway-aks-group" --query objectId -o tsv)
+AZURE_GROUP_ID=$(az ad group show --group "tangtuphuc2017" --query objectId -o tsv)
 
 echo "AZURE AD GROUP ID IS: $AZURE_GROUP_ID"
